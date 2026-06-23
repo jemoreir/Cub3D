e41b14eca232022c8d868b3e89348775f6662fc2
jean <jean5326@gmail.com>
2026-06-23 15:26:35 +0100

    check line

--- jean/valid_map.c
+++ jean/valid_map.c
@@ -1,23 +1,23 @@
 #include "cub3d.h"
 
-int check_extension(char *filename)
+int	check_extension(char *filename)
 {
-	int len;
+	int	len;
 
 	len = ft_len(filename);
 	if (!filename || len < 5)
 		return (treat_error(T_EXTENSION), 0);
-	if (filename[len - 4] != '.' || filename[len - 3] != 'c'
-		|| filename[len - 2] != 'u' || filename[len - 1] != 'b')
+	if (filename[len - 4] != '.' || filename[len - 3] != 'c' || filename[len
+		- 2] != 'u' || filename[len - 1] != 'b')
 		return (treat_error(T_EXTENSION), 0);
 	return (1);
 }
 
-int count_lines(char *file)
+int	count_lines(char *file)
 {
-	char *line;
-	int r;
-	int fd;
+	char	*line;
+	int		r;
+	int		fd;
 
 	fd = open(file, O_RDONLY);
 	r = 0;
@@ -34,7 +34,7 @@ int count_lines(char *file)
 	return (r);
 }
 
-char **file_to_array(char *filename)
+char	**file_to_array(char *filename)
 {
 	int		fd;
 	int		i;
@@ -57,4 +57,23 @@ char **file_to_array(char *filename)
 	arr[i] = NULL;
 	close(fd);
 	return (arr);
+}
+
+int	is_map_line(char *line)
+{
+	int	i;
+
+	i = 0;
+	if (!line)
+		return (0);
+	if (line[i] == '\0')
+		return (0);
+	while (line[i])
+	{
+		if (line[i] != '1' && line[i] != '0' && line[i] != 'N'
+			&& line[i] != 'W' && line[i] != 'S' && line[i] != 'E' && line[i] != ' ')
+			return (0);
+		i++;
+	}
+	return (1);
 }
